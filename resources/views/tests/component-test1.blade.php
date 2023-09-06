66427689542bc86e74867cc4d6d5ed836a1fb39e
ABenn17282002 <a.benn17282002@gmail.com>
2023-09-06 14:02:51 +0000

    名前付きSlot

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,4 +1,8 @@
 <x-tests.app>
+    {{-- view/component/tests/app.blade.php用header--}}
+    <x-slot name="header">
+        コンポーネントテスト1Heaeder
+    </x-slot>
     {{-- view/component/tests/app.blade.php用コンポーネント--}}
     コンポーネントテスト1
 </x-tests.app>
