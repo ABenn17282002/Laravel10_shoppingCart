2c87acc0bcecfb3705e78a633d0081a1bd709dd5
ABenn17282002 <a.benn17282002@gmail.com>
2023-09-22 04:47:40 +0000

    Componentの初期値設定

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -8,4 +8,7 @@
 
     {{-- cardへ値を受け渡しをするコンポーネント 変数を渡す場合は:プロパティ名="$変数"---}}
     <x-tests.card  title="Component2" content="Component2の本文" :messages="$messages"/>
+
+    {{-- components/tests/card.blade.php -@props([])から呼び出し--}}
+    <x-tests.card  title="Component1_title2" />
 </x-tests.app>
